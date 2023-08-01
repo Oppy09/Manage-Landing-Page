ee5311ca638186bb4a407d43bfd8f9304273eb96
Aderinola Opemipo Emmanuel <aderinolaopemipo@gmail.com>
2023-08-01 23:19:46 +0100

    added transitions to the nav links

--- index.js
+++
@@ -1,6 +0,0 @@
-const button = document.querySelector(".nav-btn");
-const mobileLinks = document.querySelector(".mobile-sc");
-
-button.addEventListener("click", function() {
-    mobileLinks.classList.toggle("show");
-});
